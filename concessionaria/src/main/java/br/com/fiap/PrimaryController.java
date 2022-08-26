3769a55b2812072b25e9d5b70429d3f971f52c36
llluferr <rm93205@fiap.com>
2022-08-26 12:07:14 -0300

    Versao inicial

---
+++ concessionaria/src/main/java/br/com/fiap/PrimaryController.java
@@ -0,0 +1,33 @@
+package br.com.fiap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class PrimaryController {
+
+    @FXML private TextField textFieldMarca;
+    @FXML private TextField textFieldModelo;
+    @FXML private TextField textFieldPlaca;
+    @FXML private TextField textFieldPreco;
+    @FXML private TextField textFieldAno;
+
+    @FXML private ListView lvVeiculos;
+
+    private List<Veiculo> lista = new ArrayList<>();
+
+    public void salvar(){
+        String marca = textFieldMarca.getText();
+        String modelo = textFieldModelo.getText();
+        String placa = textFieldPlaca.getText();
+        double preco = Double.valueOf(textFieldPreco.getText() );
+        int ano = Integer.valueOf(textFieldAno.getText());
+
+        Veiculo veiculo = new Veiculo(marca, modelo, placa, preco, ano);
+        lista.add(veiculo);
+        lvVeiculos.getItems().addAll(lista);
+    }
+}
---
+++ concessionaria/src/main/java/br/com/fiap/Veiculo.java
@@ -0,0 +1,42 @@
+package br.com.fiap;
+
+public class Veiculo {
+    //Model BO POJO Entity Resource
+    //atributos 
+    private String marca;
+    private String modelo;
+    private String placa;
+    private double preco;
+    private int ano;
+    public Veiculo(String marca, String modelo, String placa, double preco, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.placa = placa;
+        this.preco = preco;
+        this.ano = ano;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    @Override
+    public String toString() {
+        return "Veiculo [ano=" + ano + ", marca=" + marca + ", modelo=" + modelo + ", placa=" + placa + ", preco="
+                + preco + "]";
+    }
+    
+    //metodos
+    
+}
